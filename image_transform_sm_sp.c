4b598306842d3ed2242dbc3ad2bb56f660f7f75d
ElliottBradford <36458406+ElliottBradford@users.noreply.github.com>
2020-10-15 18:28:30 -0500

    Add files via upload

---
+++ image_transform_sm_sp.c
@@ -0,0 +1,63 @@
+//Import libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main(){
+	//Declare variables
+	FILE *image, *outputImage, *lecturas;
+    image = fopen("sample.bmp","rb");          //Imagen original a transformar
+    outputImage = fopen("img3_dd.bmp","wb");    //Imagen transformada
+    long ancho;
+    long alto;
+    unsigned char r, g, b;               //Pixel
+    double t1, t2, time;
+    int pivot = 0;
+    unsigned char xx[54];
+    
+    //Check headers
+    for(int i=0; i<54; i++) {
+      xx[i] = fgetc(image);
+      fputc(xx[i], outputImage);   //Copia cabecera a nueva imagen
+    }
+    
+    ancho = (long)xx[21]*(long)xx[20]*65536+(long)xx[19]*256+(long)xx[18];
+    alto = (long)xx[24]*65536+(long)xx[23]*256+(long)xx[22];
+
+    //Check extra elements
+    while(((ancho*3)+pivot)%4 != 0){
+        pivot++;
+    }
+
+    //Transform the image
+    int count = 0;
+    t1 = omp_get_wtime();
+    for(int i = 0; i < ((ancho*3)+pivot)*alto; i+=3){
+            b = fgetc(image);
+            g = fgetc(image);
+            r = fgetc(image);
+            unsigned char pixel = 0.21*r+0.72*g+0.07*b;
+            fputc(pixel, outputImage);
+            fputc(pixel, outputImage);
+            fputc(pixel, outputImage);
+            
+            count += 3;
+            if(count == ancho*3){
+				for(int j = 0; j < pivot; j++){
+					fputc(fgetc(image), outputImage);
+				}
+                i+=pivot;
+                count = 0;
+            }
+    }
+    
+    t2 = omp_get_wtime();
+    time = t2 - t1;
+	printf("Time = %f\n",time);
+    
+    //Close files
+    fclose(image);
+    fclose(outputImage);
+    
+	return 0;
+}
